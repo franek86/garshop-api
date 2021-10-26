04132c48112331ec1b5ad7acb777845bc6faca6c
franek86 <frane.konjevoda@gmail.com>
2021-10-26 21:16:30 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '03d73b7531f1375f98de71aee64bb41d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '10b74347-6252-4788-bec6-960c2665b186'
+};
